01a8f211ec64bdacd18c05a5093b042e6872153d
amuthu1996 <amuthu1996@gmail.com>
2015-10-26 16:31:59 +0530

    devise installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, ActiveAdmin::Devise.config
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+  
   get 'report/index'
 
   get 'report/student'
